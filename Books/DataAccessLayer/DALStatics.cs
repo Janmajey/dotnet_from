4e58b5560f50fe02bc42322a113c376619e5f5ec
Janmajey <janmajeynathvani@gmail.com>
2024-05-28 10:03:47 +0530

    using dotnet login page with authentication and autorization

---
+++ Books/DataAccessLayer/DALStatics.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DALStatics
+    {
+        public static List<UserInfo> Users = new List<UserInfo>()
+        {
+            new UserInfo()
+            {
+                Username = "jmn_admin",
+                EmailAddress = "jmn.admin@tatvasoft.com",
+                Password = "jmn123",
+                GivenName = "Janmajey",
+                Surname = "Nathvani",
+                Role = "Administrator"
+            },
+
+            new UserInfo()
+            {
+                Username = "smit_reader",
+                EmailAddress = "smit.user@tatvasoft.com",
+                Password = "smit123",
+                GivenName = "Smit",
+                Surname = "Namha",
+                Role = "BookUser"
+            },
+        };
+    }
+}
